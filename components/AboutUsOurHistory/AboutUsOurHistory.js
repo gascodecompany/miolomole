886dc5eee5b607753ca0c72bb6f3f758fde678ab
Tiago <tiago.kise@hotmail.com>
2021-02-21 18:45:28 -0300

    AboutUsOurHistory

--- components/AboutUsOurHistory/AboutUsOurHistory.js
+++ components/AboutUsOurHistory/AboutUsOurHistory.js
@@ -1,19 +1,21 @@
 import * as S from './AboutUsOurHistory.style'
+import Container from '../Container'
 
 export default function AboutUsOurHistory(){
 
  return(
     <S.AboutUsOurHistory>
+      <Container>
       <S.OurHistoryWrapper>
         <S.OurHistoryTitle>A história de quem somos</S.OurHistoryTitle>
-        <S.OurHistoryText>Miolo Mole nasceu em 2010 como um projeto de mediação de leitura.
-          <br/>Entre 2010 e 2013 foram realizadas oficinas de curta e média duração, com crianças e pré-adolescentes, em locais de alta vulnerabilidade social, livrarias e espaços culturais diversos na cidade de São Paulo.
+        <S.OurHistoryText>Miolo Mole nasceu em 2010 como um projeto de mediação de leitura. Entre 2010 e 2013 foram realizadas oficinas de curta e média duração, com crianças e pré-adolescentes, em locais de alta vulnerabilidade social, livrarias e espaços culturais diversos na cidade de São Paulo.
           <br/>Essa vivência trouxe à editora e idealizadora do projeto, Luana Vignon, um amplo repertório de narrativas e experiências de leitura.
           <br/>
           <br/>O desejo de publicar obras que valorizassem a diversidade e a pluralidade cultural, preservando a poética da infância, foi o estímulo para a criação do selo literário Miolo Mole. No ano de 2019 uniram-se a essa ideia o editor Marco Saliba e o designer gráfico Daniel Rosa, ocasião em que a editora passou a integrar o Grupo Eureka. Neste ano foram iniciados os primeiros projetos com autores e ilustradores convidados.
         </S.OurHistoryText>
+        <span className='grayBox'></span>
       </S.OurHistoryWrapper>
-      <span></span>
+      </Container>
     </S.AboutUsOurHistory>
   )
 }
--- components/AboutUsOurHistory/AboutUsOurHistory.style.js
+++ components/AboutUsOurHistory/AboutUsOurHistory.style.js
@@ -1,14 +1,62 @@
 import styled from 'styled-components';
 
 export const AboutUsOurHistory = styled.div`
-  
+  margin-bottom: 130px;
+
+  .container{
+    align-items: end;
+
+    @media screen{
+      @media (min-width: 1024px){
+        padding: 0;
+      }
+    }
+    .grayBox{
+      width: 325px;
+      height: 515px;
+      background: #D8D8D8;
+      position: absolute;
+      right: -160px;
+      bottom: -90px;
+    }
+  }
 `;
-export const OurHistoryTitle = styled.div`
 
-`
-export const OurHistoryText = styled.div`
-
-`
 export const OurHistoryWrapper = styled.div`
   background: #333333;
+  width: 84%;
+  /* margin: 0 auto; */
+  padding: 50px;
+  margin-top: -180px;
+  position: relative;
+  ::before{
+    content: '';
+    position: absolute;
+    width: 40%;
+    height: 100%;
+    top: 0px;
+
+  }
+
+
+`
+
+export const OurHistoryTitle = styled.div`
+  font-family: Amatic SC;
+  font-weight: bold;
+  font-size: 75px;
+  letter-spacing: 2.4px;
+  color: #00A79D;
+  width: 80%;
+  margin-bottom: 20px;
+`
+
+export const OurHistoryText = styled.div`
+  font-family: Montserrat;
+  font-weight: 500;
+  font-size: 17px;
+  line-height: 30px;
+  color: #FFFFFF;
+  width: 80%;
+
 `
