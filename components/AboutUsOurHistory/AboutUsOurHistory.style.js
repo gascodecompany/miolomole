19d085b9e199eb010d9179d30489002688318567
Tiago <tiago.kise@hotmail.com>
2021-02-21 15:32:41 -0300

    Start AboutUsOurHistory

---
+++ components/AboutUsOurHistory/AboutUsOurHistory.style.js
@@ -0,0 +1,14 @@
+import styled from 'styled-components';
+
+export const AboutUsOurHistory = styled.div`
+  
+`;
+export const OurHistoryTitle = styled.div`
+
+`
+export const OurHistoryText = styled.div`
+
+`
+export const OurHistoryWrapper = styled.div`
+  background: #333333;
+`
--- pages/quem-somos/AboutUs.js
+++ pages/quem-somos/AboutUs.js
@@ -1,10 +1,12 @@
 import AboutUsJumbotron from "../../components/AboutUsJumbotron";
+import AboutUsOurHistory from "../../components/AboutUsOurHistory";
 
 export default function AboutUs(){
 
  return(
     <div>
       <AboutUsJumbotron/>
+      <AboutUsOurHistory/>
     </div>
   )
 }
