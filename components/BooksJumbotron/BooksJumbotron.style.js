d4ed8587b204f6e37d0774db1b2d8ea3f0e8f295
Tiago <tiago.kise@hotmail.com>
2021-02-23 21:12:27 -0300

    removing padding-inline-start from jumbotron slides

--- components/BooksJumbotron/BooksJumbotron.style.js
+++ components/BooksJumbotron/BooksJumbotron.style.js
@@ -9,6 +9,7 @@ export const BooksJumbotronContainer = styled.div`
       display: flex;
       align-items: center;
       justify-content: center;
+      padding-inline-start: 0px;
 
       li{
         width: 170px;
--- components/HomeJumbotron/HomeJumbotron.style.js
+++ components/HomeJumbotron/HomeJumbotron.style.js
@@ -9,6 +9,8 @@ export const HomeJumbotronContainer = styled.div`
       display: flex;
       align-items: center;
       justify-content: center;
+      padding-inline-start: 0px;
+
 
       li{
         width: 170px;
