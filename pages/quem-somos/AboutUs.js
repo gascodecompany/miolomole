500b2d59b88b02d06aefe0bb8b170986cfde270b
Tiago <tiago.kise@hotmail.com>
2021-03-14 16:16:22 -0300

    Ordem dos componentes do AboutUs

--- pages/quem-somos/AboutUs.js
+++ pages/quem-somos/AboutUs.js
@@ -13,8 +13,8 @@ export default function AboutUs(){
       <AboutUsOurHistory/>
       <AboutUsQuote/>
       <AboutUsSectionOne/>
-      <AboutUsSlider/>
       <AboutUsImagination/>
+      <AboutUsSlider/>
     </div>
   )
 }
