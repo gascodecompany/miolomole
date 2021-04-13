c45d3346ea4dfa6651ac52a10add1b6576826209
Tiago <tiago.kise@hotmail.com>
2021-04-13 20:31:54 -0300

    editor

--- pages/blog/[id]/Post.js
+++ pages/blog/[id]/Post.js
@@ -2,7 +2,13 @@ import { useRouter } from 'next/router'
 import PostJumbotron from '../../../components/PostJumbotron'
 import LatestArticles from '../../../components/LatestArticles'
 import pt from "../../../i18n/pt";
+// import BlogEditor from '../../../components/BlogEditor';
+import dynamic from 'next/dynamic'
 
+const BlogEditor = dynamic(
+  () => import('../../../components/BlogEditor'),
+  { ssr: false }
+)
 export default function Post(){
   const router = useRouter()
   const { id } = router.query
@@ -11,6 +17,7 @@ export default function Post(){
  return(
     <>
       <PostJumbotron id={id}/>
+      <BlogEditor/>
       <LatestArticles items={t.BLOG_ARTICLES}/>
     </>
   )
