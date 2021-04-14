bae00a3bf46b687582924a4295181628d089768d
Pedro Raimondi <pedroksk8@hotmail.com>
2021-04-14 11:55:13 -0300

    change de erros

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,6 @@
 import { ThemeProvider } from 'styled-components';
 import { mioloMoleAppTheme } from '../styles/global';
-import { appWrapper } from './appProvider';
+import { appWrapper } from '../utils/appProvider';
 import Footer from "../components/Footer";
 import Header from "../components/Header";
 import LogoutButton from '../components/LogoutButton';
--- pages/admin/admin.js
+++ pages/admin/admin.js
@@ -1,5 +1,5 @@
 import LoginModal from "../../components/LoginModal";
-import { appWrapper } from "../appProvider";
+import { appWrapper } from "../../utils/appProvider";
 
 export default function Admin(props){
 
