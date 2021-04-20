b30c94db86cf24768cd8d93bf80eb8eefba8b1a6
Pedro Raimondi <pedroksk8@hotmail.com>
2021-04-19 22:21:11 -0300

    fix: text api

--- pages/api/textos/index.js
+++ pages/api/textos/index.js
@@ -4,43 +4,44 @@ import Text from '../../../models/text';
 const textos = async (req, res) => {
   const { method } = req;
 
-  switch (method) {
-    case 'GET':
-      try {
-        const texts = await Text.find({ page: req.query.page });
-        return res.status(200).send(texts);
-      } catch (err) { return res.status(500).send(err.message) }
-    case 'PUT':
-      try {
-        const { textKey, text, editedBy, page } = req.body;
-        if(!textKey && !text) { return res.status(422).send('Dados Incompletos') };
+  try{
+    switch (method) {
+      case 'GET':
         try {
-          var oldText = await Text.findOne({ textKey: textKey });
-          if (oldText) {
-            oldText.text = text;
-            oldText.editedBy = editedBy;
-            var oldTextUpdated = await oldText.save();
-            return res.status(200).send(oldTextUpdated);
-          } else {
+          const texts = await Text.find({ page: req.query.page });
+          res.status(200).json({ texts });
+        } catch (err) { res.status(500).json({ errorMessage: err.message }) }
+      case 'PUT':
+        try {
+          const { textKey, text, editedBy, page } = req.body;
+          if(!textKey && !text) { res.status(422).json({ errorMessage: 'Dados Incompletos' }) };
+          try {
+            var oldText = await Text.findOne({ textKey: textKey });
+            if (oldText) {
+              oldText.text = text;
+              oldText.editedBy = editedBy;
+              var oldTextUpdated = await oldText.save();
+              res.status(200).json({ oldTextUpdated });
+            } else {
+              var newText = new Text({ textKey, text, page, editedBy });
+              var textCreated = await newText.save();
+              res.status(200).json({ textCreated });
+            }
+          } catch (err) { res.status(500).json({ errorMessage: err.message }) }
+        } catch (err) { res.status(500).json({ errorMessage: err.message }) }
+      case 'POST': 
+        const { textKey, text, page, editedBy } = req.body;
+        if (textKey && text && page) {
+          try {
             var newText = new Text({ textKey, text, page, editedBy });
             var textCreated = await newText.save();
-            return res.status(200).send(textCreated);
-          }
-        } catch (err) { return res.status(500).send(err.message) }
-      } catch (err) { return res.status(500).send(err.message) }
-    case 'POST': 
-      const { textKey, text, page, editedBy } = req.body;
-      if (textKey && text && page) {
-        try {
-          var newText = new Text({ textKey, text, page, editedBy });
-          var textCreated = await newText.save();
-          return res.status(200).send(textCreated);
-        } catch (error) { return res.status(500).send(error.message) }
-      } else { return res.status(422).send('data_incomplete'); }
-    default:
-      // res.setHeader('Allow', ['GET', 'PUT'])
-      return res.status(405).end(`Method ${method} Not Allowed`)
-  }
+            res.status(200).json({ textCreated });
+          } catch (error) { res.status(500).json({ errorMessage: error.message }) }
+        } else { res.status(422).json({ errorMessage: 'data_incomplete' }); }
+      default:
+        res.status(405).json({ errorMessage: `Method ${method} Not Allowed` })
+    }
+  } catch (err) { res.end() }
   
 };
 
